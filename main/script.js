06cb41ccf2d4e638c895e6b4b84457b2773a54bb
Md Aftab Uddin <aftabhasan7856@gmail.com>
2025-08-31 09:17:42 +0600

    heart count done

---
+++ main/script.js
@@ -0,0 +1,54 @@
+document.getElementById("heart1").addEventListener("click", function() {
+    let countlabel = document.getElementById("heart-count");
+    let count = parseInt(countlabel.textContent);
+    count++;
+    countlabel.textContent = count;
+});
+document.getElementById("heart2").addEventListener("click", function() {
+    let countlabel = document.getElementById("heart-count");
+    let count = parseInt(countlabel.textContent);
+    count++;
+    countlabel.textContent = count;
+});
+document.getElementById("heart3").addEventListener("click", function() {
+    let countlabel = document.getElementById("heart-count");
+    let count = parseInt(countlabel.textContent);
+    count++;
+    countlabel.textContent = count;
+});
+document.getElementById("heart4").addEventListener("click", function() {
+    let countlabel = document.getElementById("heart-count");
+    let count = parseInt(countlabel.textContent);
+    count++;
+    countlabel.textContent = count;
+});
+document.getElementById("heart5").addEventListener("click", function() {
+    let countlabel = document.getElementById("heart-count");
+    let count = parseInt(countlabel.textContent);
+    count++;
+    countlabel.textContent = count;
+});
+document.getElementById("heart6").addEventListener("click", function() {
+    let countlabel = document.getElementById("heart-count");
+    let count = parseInt(countlabel.textContent);
+    count++;
+    countlabel.textContent = count;
+});
+document.getElementById("heart7").addEventListener("click", function() {
+    let countlabel = document.getElementById("heart-count");
+    let count = parseInt(countlabel.textContent);
+    count++;
+    countlabel.textContent = count;
+});
+document.getElementById("heart8").addEventListener("click", function() {
+    let countlabel = document.getElementById("heart-count");
+    let count = parseInt(countlabel.textContent);
+    count++;
+    countlabel.textContent = count;
+});
+document.getElementById("heart9").addEventListener("click", function() {
+    let countlabel = document.getElementById("heart-count");
+    let count = parseInt(countlabel.textContent);
+    count++;
+    countlabel.textContent = count;
+});
